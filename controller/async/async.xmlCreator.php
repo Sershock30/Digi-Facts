e2378842c6cd27fd59b82a98965839e0487f3b37
Sershock30 <sergio.chang@hotmail.com>
2018-02-06 11:06:01 -0600

    Update a funcion de async

--- controller/async/async.xmlCreator.php
+++ controller/async/async.xmlCreator.php
@@ -16,51 +16,43 @@
 	// Los 2 prints son lo que recive el responde, puede verlo en la consola de js
 	$xml_main = $xmlCreator->createXML();
 	$xml_encoded = base64_encode($xml_main);
-	// $test = simplexml_load_string($xml_main);
-	// $json = json_encode($test);
-	// $encryp = base64_encode($xml_main);
-	// $desencrypt = base64_decode($encryp);
-	// print_r($encryp);
-	// print_r($desencrypt);
 
-	$Array = [
-		"clave"=>"50617011800100001010000000001156451223",
-		"fecha"=>"2018-01-01T00:00:00-0600",
-		"emisor"=>[
-			"tipoIdentificacion"=>"02",
-			"numeroIdentificacion"=>"116610374"
-		],
-		"receptor"=>[
-			"tipoIdentificacion"=>"02",
-			"numeroIdentificacion"=>"116610374"
-		],
-		"comprobanteXml"=>$xml_encoded
-	];
 
-	$response['data'] = $Array;
+	// $Array = [
+	// 	"clave"=>"50617011800100001010000000001156451223",
+	// 	"fecha"=>"2018-01-01T00:00:00-0600",
+	// 	"emisor"=>[
+	// 		"tipoIdentificacion"=>"02",
+	// 		"numeroIdentificacion"=>"116610374"
+	// 	],
+	// 	"receptor"=>[
+	// 		"tipoIdentificacion"=>"02",
+	// 		"numeroIdentificacion"=>"116610374"
+	// 	],
+	// 	"comprobanteXml"=>$xml_encoded
+	// ];
+
+	// $response['data'] = $Array;
 
 	//Aqui se procede a ejecutar el Envio del Array a Hacienda, seguido de esto, con la respuesta que entregue el API,
 	// se valida si fue correcto o incorrecto.
+	include '../../libs/HACIENDA_API/class.HaciendaAPI.php';
 
-	include '../../libs/OpenID/apiKey.php';
-	include "../../libs/OpenID/class.OpenId.php";
+	//require_once '../../module/class.conexion.php';
+	//require_once '../../module/class.metodos.php';
 
-	//se instancia la clase de libreria OpenID
-	//El host es el servidor que se usara como Gateway
-	// $OpenID = new LightOpenID("localhost");
+	//Se instancia el objeto de HACIENDA_API
+	//Se instancia la clase de metodos
+	//$M = new Metodos();
 
+	//desde metodos se obtiene el consecutivo del usuario que inició sesion.
 
-	// if (!$OpenID->mode) {
-	// 	$OpenID->identity = "https://api.comprobanteselectronicos.go.cr/recepcion-sandbox/v1/";
-	// 	$response['StatusHacienda'] = true;
-	// 	$response['msgHacienda'] = "Conexión exitosa al API de Hacienda";
-	// }else if($OpenID->mode == "cancel"){
-	// 	$response['customMsg'] = "Se ha cancelado la operacion.";
-	// }else{
-	// 	$response['msgHacienda'] = "Error al autenticar con el Token"; 
-	// }
+	$hacienda = new HaciendaAPI();
 
-	//Se establece el Data del response como la respuesta al servidor
+	// se llama  la funcion para obtener el Token
+	
+	//$token = $hacienda->get_Token();
+	//$hacienda->send_invoice($xml_encoded, $token, $consec);
 
 	echo json_encode($response);
 
---
+++ libs/HACIENDA_API/class.HaciendaAPI.php
@@ -0,0 +1,82 @@
+<?php 
+
+class HaciendaAPI{
+
+    public function get_Token(){
+        $url = 'https://idp.comprobanteselectronicos.go.cr/auth/realms/rut-stag/protocol/openid-connect/token';//access token url
+        $data = array('client_id' => 'api-stag',//Test: 'api-stag' Production: 'api-prod'
+                      'client_secret' => '',//always empty
+                      'grant_type' => 'password', //always 'password'
+                      //go to https://www.hacienda.go.cr/ATV/login.aspx to generate a username and password credentials
+                      'username' => 'ATV user', 
+                      'password' => 'ATV pass', 
+                      'scope' =>'');//always empty
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if ($result === FALSE) { echo $result; }
+        $token = json_decode($result); //get a token object
+        return $token; //return a json object whith token and refresh token
+    }
+
+    //send invoice xml to Hacienda API
+    public function send_invoice($xml, $token, $consec) {
+
+        //aqui vamos a accesar a la BD y obtener el consecutivo del usuario
+        $consecutive = $consec;
+
+        //aqui vamos a generar la llave del documento
+        $key = $this->generate_key($consecutive); //key invoice number
+
+        //aqui se cambia el invoice por el XML Generado por el class.xmlCreator.php
+        $invoice = $xml;//create a xml string
+
+        //Aqui se envía el Token obtenido de la funcion de get_Token
+        $authToken = $token;//get OAuth2.0 token
+
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => "https://api.comprobanteselectronicos.go.cr/recepcion-sandbox/v1/recepcion",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 30,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "POST",
+          CURLOPT_POSTFIELDS => "{\n\t\"clave\": \"$key\","
+            . "\n\t\"fecha\": \"2017-10-03T00:00:00-0600\","
+            . "\n\t\"emisor\": {\n\t\t\"tipoIdentificacion\": \"02\",\n\t\t\"numeroIdentificacion\": \"3101123456\"\n\t},"
+            . "\n\t\"receptor\": {\n\t\t\"tipoIdentificacion\": \"02\",\n\t\t\"numeroIdentificacion\": \"3101123456\"\n\t},"
+            . "\n\t\"callbackUrl\": \"https://example.com/invoiceView\","
+            . "\n\t\"comprobanteXml\": \"$invoice\"\n}",
+          CURLOPT_COOKIE => "__cfduid=d73675273d6c68621736ad9329b7eff011507562303",
+          CURLOPT_HTTPHEADER => array(
+            "authorization: Bearer ".$authToken ,
+            "content-type: application/json"
+          ),
+        ));
+    
+        $data = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+
+        if ($err) {
+          //echo "cURL Error #:" . $err;
+          return "Error";
+        } else {
+
+          //se le cambia el nombre a data para no tener errores con el response de AJAX
+          return json_decode($data);
+        }
+    }
+
+}
