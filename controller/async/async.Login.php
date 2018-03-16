b2da0b91462eed63461036aa76539b9b33ee2c35
Sershock30 <sershock30@hotmail.com>
2018-03-16 13:20:25 -0600

    Update PHP clases y async

--- controller/async/async.Login.php
+++ controller/async/async.Login.php
@@ -2,23 +2,34 @@
 
 include "../../config_files/config.php";
 
+$response = [
+	"status"=>"",
+	"msg"=>"",
+	"data"=>[],
+	"user_view"=>"",
+	"ses_timeout"=""
+];
+
 if (isset($_POST['email']) && isset($_POST['pass'])) {
 	
 	//se incluyen los archivos necesarios
 	require_once('../../module/class.conexion.php');
 	require_once('../../module/class.metodos.php');
-	require_once('../class/class.Login.php');
+	require_once('../../module/class/class.MLogin.php');
 
-	$log = new Login();
-	$data = $log->userLogin($_POST['email'], $_POST['pass']);
+	$Login = new Login();
+	$data = $Login->userLogin($_POST['email'], $_POST['pass']);
 
 	//se valida si los datos del select son nulos.
 	if (!is_null($data)) {
 		//se inicia la sesion
 		session_start();
+
 		//se establecen las variables de sesion
+		$_SESSION['ADMIN'] = $data['admin'];
 		$_SESSION['ID'] = $data['id_usuario'];
-		$_SESSION['TIPO'] = $data['id_tipo'];
+		$_SESSION['CORREO'] = $data['correo'];
+		$_SESSION['IDE'] = $data['identificacion'];
 
 		//se redirecciona a la pagina del administrador
 		header('location:../../'.admin_viewPath);
@@ -28,8 +39,11 @@ if (isset($_POST['email']) && isset($_POST['pass'])) {
 	}
 } else {
 	//se redirecciona al login junto con el mensaje de error
-	header('location: ../../'.admin_viewPath.'?msg=Favor llenar los campos necesarios');
+	$response["status"] = false;
+	$response["msg"] = "Error al recibir los datos.";
 }
+
+echo json_encode($response);
 
 
 
